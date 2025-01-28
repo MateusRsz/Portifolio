bfb551ece85280f42c9412db1027767f6243c4a3
MateusRsz <matheusestudante69@gmail.com>
2025-01-27 23:56:57 -0300

    sons entre outros

--- Pomodoro/index2.js
+++ Pomodoro/index2.js
@@ -1,23 +1,48 @@
 
 let displai = document.querySelector('#display')
+const click = new Audio('audio/click.mp3')
+const end = new Audio('audio/end.mp3')
+click.preload = 'auto'
+end.preload = 'auto'
+
 var minute = 0
 var second = 0
-displai.textContent = `${minute}:${second}`
 
+
+displai.textContent = `${minute}:${second}`
 class pomodoro {
     static propriedade = null
+
+    static Alert() {
+        let div = document.getElementById('alert')
+        div.style.display = 'block'
+        setTimeout(()=>{
+            div.style.transition = 'opacity 3s'
+            div.style.opacity = '0%'
+        }, 10000)
+       
+    }
+    static audioclick() {
+        click.play()
+        click.currentTime = 0
+    }
+
     static moreMinute() {
+        this.audioclick()
         minute++
         displai.textContent = `${minute}:${second}`
     }
     static lessMinute() {
+        this.audioclick()
         minute--
         displai.textContent = `${minute}:${second}`
     }
     static start() {
+        this.audioclick()
         this.propriedade = setInterval(() => {
             if (minute == 0 && second == 0) {
-                alert('descanso')
+                this.Alert()
+                end.play()
                 minute = 5
                 setTimeout(() => {
                     alert('fim do pomodoro')
@@ -31,10 +56,7 @@ class pomodoro {
                 second = 59
             }
             displai.textContent = `${minute}:${second}`
-
         }, 1000)
-
-
     }
     static reset() {
         clearInterval(this.propriedade)
@@ -42,14 +64,12 @@ class pomodoro {
         minute = 0
         second = 0
         displai.textContent = `${minute}:${second}`
+        this.audioclick()
     }
-
 }
-
 document.querySelector('#more_time').addEventListener('click', () => {
     pomodoro.moreMinute()
 })
-
 document.querySelector('#less_time').addEventListener('click', () => {
     if (minute == 0) {
         alert('Ação indisponivel')
@@ -57,15 +77,12 @@ document.querySelector('#less_time').addEventListener('click', () => {
         pomodoro.lessMinute()
     }
 })
-
 document.querySelector('#start').addEventListener('click', () => {
     if (minute == 0 && second == 0) {
         alert("Minute required more when one or igual")
     } else { pomodoro.start() }
 })
-
 document.querySelector('#reset').addEventListener('click', () => {
     pomodoro.reset()
 })
-
 
